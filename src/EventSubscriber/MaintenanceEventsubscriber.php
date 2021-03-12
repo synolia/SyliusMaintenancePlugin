3c7996e513c49448699b7f6e5ea207c98d4199c3
Trinh_Vo <trinh.vo@synolia.com>
2021-03-12 11:58:30 +0100

    implement MaintenanceEventsubscriber

--- src/EventSubscriber/MaintenanceEventsubscriber.php
+++ src/EventSubscriber/MaintenanceEventsubscriber.php
@@ -4,7 +4,38 @@ declare(strict_types=1);
 
 namespace Synolia\SyliusMaintenancePlugin\EventSubscriber;
 
-final class MaintenanceEventsubscriber
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+final class MaintenanceEventsubscriber implements EventSubscriberInterface
 {
-    // TODO : show message if maintenance.yaml exists
+    private const PREFIX = 'admin';
+
+    /** @var Filesystem */
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            RequestEvent::class => 'handle',
+        ];
+    }
+
+    public function handle(RequestEvent $event): void
+    {
+        $getRequestUri = $event->getRequest()->getRequestUri();
+        if ($this->filesystem->exists(\dirname(__DIR__) . '/Resources/config/maintenance.yaml')) {
+            if (strpos($getRequestUri, self::PREFIX, 1) === false) {
+                $response = new JsonResponse('site en maintenance !!!');
+                $event->setResponse($response);
+            }
+        }
+    }
 }
