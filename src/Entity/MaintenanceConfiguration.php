c6f26b9b196b86be33ba983dcb2d49cdfb278fd0
Trinh_Vo <trinh.vo@synolia.com>
2021-04-08 14:37:25 +0200

    refactor MaintenanceConfiguration.php

--- src/Entity/MaintenanceConfiguration.php
+++ src/Entity/MaintenanceConfiguration.php
@@ -14,43 +14,36 @@ use Sylius\Component\Resource\Model\ResourceInterface;
 class MaintenanceConfiguration implements ResourceInterface
 {
     /**
-     * @var int
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private int $id;
 
-    /**
-     * @var string|null
-     * @ORM\Column(type="string", length=255)
-     */
-    private $ipAddresses = '';
+    /** @ORM\Column(type="string") */
+    private string $ipAddresses = '';
 
-    /**
-     * @var bool
-     * @ORM\Column(type="boolean", nullable=false)
-     */
-    private $enabled = true;
+    /** @ORM\Column(type="boolean") */
+    private bool $enabled = true;
 
-    /**
-     * @var string|null
-     * @ORM\Column(type="text")
-     */
-    private $customMessage = '';
+    /** @ORM\Column(type="text") */
+    private string $customMessage = '';
 
-    public function getId(): ?int
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getIpAddresses(): ?string
+    public function getIpAddresses(): string
     {
         return $this->ipAddresses;
     }
 
     public function setIpAddresses(?string $ipAddresses): self
     {
+        if (null === $ipAddresses) {
+            return $this;
+        }
         $this->ipAddresses = $ipAddresses;
 
         return $this;
@@ -61,20 +54,23 @@ class MaintenanceConfiguration implements ResourceInterface
         return $this->enabled;
     }
 
-    public function setEnabled(?bool $enabled): self
+    public function setEnabled(bool $enabled): self
     {
-        $this->enabled = (bool) $enabled;
+        $this->enabled = $enabled;
 
         return $this;
     }
 
-    public function getCustomMessage(): ?string
+    public function getCustomMessage(): string
     {
         return $this->customMessage;
     }
 
     public function setCustomMessage(?string $customMessage): self
     {
+        if (null === $customMessage) {
+            return $this;
+        }
         $this->customMessage = $customMessage;
 
         return $this;
