d49168b08e91eececebdc3d30a7acac2d51e828e
Olivier ALLAIN <olivier.allain@synolia.com>
2021-05-25 17:20:53 +0200

    activate ECS and fix code

--- ruleset/ecs.php
+++ ruleset/ecs.php
@@ -3,14 +3,16 @@
 declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
+use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__. '/../vendor/sylius-labs/coding-standard/ecs.php');
+    $containerConfigurator->import(__DIR__ . '/../vendor/sylius-labs/coding-standard/ecs.php');
 
     $services = $containerConfigurator->services();
 
     $services->set(OrderedClassElementsFixer::class);
+    $services->set(BinaryOperatorSpacesFixer::class);
 
     $parameters = $containerConfigurator->parameters();
 
