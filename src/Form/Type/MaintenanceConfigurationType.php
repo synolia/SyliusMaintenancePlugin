21653e7782f4be32807d72a2ddb9cf77d6cc104c
Olivier ALLAIN <olivier.allain@synolia.com>
2021-08-11 18:17:16 +0200

    fix ecs

--- src/Form/Type/MaintenanceConfigurationType.php
+++ src/Form/Type/MaintenanceConfigurationType.php
@@ -7,7 +7,6 @@ namespace Synolia\SyliusMaintenancePlugin\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
