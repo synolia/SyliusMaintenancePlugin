4b24f878182db30f4adeafe4e2e55497f0d4e7f8
Olivier ALLAIN <olivier.allain@synolia.com>
2023-01-28 23:43:47 +0100

    better test Token in Session

--- src/Storage/TokenStorage.php
+++ src/Storage/TokenStorage.php
@@ -4,24 +4,24 @@ declare(strict_types=1);
 
 namespace Synolia\SyliusMaintenancePlugin\Storage;
 
-use Sylius\Component\Resource\Storage\StorageInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 final class TokenStorage
 {
     public const MAINTENANCE_TOKEN_NAME = 'synolia_maintenance_token';
 
-    public function __construct(private StorageInterface $storage)
+    public function __construct(private RequestStack $requestStack)
     {
     }
 
     public function set(string $token): void
     {
-        $this->storage->set(self::MAINTENANCE_TOKEN_NAME, $token);
+        $this->requestStack->getSession()->set(self::MAINTENANCE_TOKEN_NAME, $token);
     }
 
     public function get(): string
     {
-        $token = $this->storage->get(self::MAINTENANCE_TOKEN_NAME);
+        $token = $this->requestStack->getSession()->get(self::MAINTENANCE_TOKEN_NAME);
         if (null === $token || !is_string($token)) {
             $token = '';
         }
