aef67f437ac7d5d17a39b1a8eabcdbb9473c83c0
Olivier ALLAIN <olivier.allain@synolia.com>
2023-09-09 14:42:43 +0200

    always use template

--- src/EventSubscriber/MaintenanceEventsubscriber.php
+++ src/EventSubscriber/MaintenanceEventsubscriber.php
@@ -7,7 +7,6 @@ namespace Synolia\SyliusMaintenancePlugin\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Synolia\SyliusMaintenancePlugin\Factory\MaintenanceConfigurationFactory;
 use Synolia\SyliusMaintenancePlugin\Voter\IsMaintenanceVoterInterface;
 use Twig\Environment;
@@ -15,7 +14,6 @@ use Twig\Environment;
 final class MaintenanceEventsubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private TranslatorInterface $translator,
         private Environment $twig,
         private MaintenanceConfigurationFactory $configurationFactory,
         private IsMaintenanceVoterInterface $isMaintenanceVoter,
@@ -37,13 +35,9 @@ final class MaintenanceEventsubscriber implements EventSubscriberInterface
             return;
         }
 
-        $responseContent = $this->translator->trans('maintenance.ui.message');
-
-        if ('' !== $configuration->getCustomMessage()) {
-            $responseContent = $this->twig->render('@SynoliaSyliusMaintenancePlugin/maintenance.html.twig', [
-                'custom_message' => $configuration->getCustomMessage(),
-            ]);
-        }
+        $responseContent = $this->twig->render('@SynoliaSyliusMaintenancePlugin/maintenance.html.twig', [
+            'custom_message' => $configuration->getCustomMessage(),
+        ]);
 
         $event->setResponse(new Response($responseContent, Response::HTTP_SERVICE_UNAVAILABLE));
     }
