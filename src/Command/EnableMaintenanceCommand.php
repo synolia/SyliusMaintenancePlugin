09f2833f83cce10740e35de11a4c0a60e0ba9306
tim.messaoudene <tim.messaoudene@synolia.com>
2023-11-14 14:02:20 +0100

    SYLIUS-274: add github workflow configs

--- src/Command/EnableMaintenanceCommand.php
+++ src/Command/EnableMaintenanceCommand.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Synolia\SyliusMaintenancePlugin\Command;
 
-use App\Entity\Channel\Channel;
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,7 +28,7 @@ final class EnableMaintenanceCommand extends Command
         private MaintenanceConfigurationExporter $maintenanceExporter,
         private MaintenanceConfigurationFactory $configurationFactory,
         private CacheInterface $synoliaMaintenanceCache,
-        private ChannelRepositoryInterface $channelRepository
+        private ChannelRepositoryInterface $channelRepository,
     ) {
         parent::__construct();
     }
@@ -52,18 +52,20 @@ final class EnableMaintenanceCommand extends Command
         return 0;
     }
 
-    private function getChannels():array
+    private function getChannels(): array
     {
         $channels = $this->channelRepository->findAll();
         $channelToExport = [];
-        /** @var Channel $channel */
+
+        /** @var ChannelInterface $channel */
         foreach ($channels as $channel) {
             $channelToExport[] = $channel->getCode();
         }
+
         return $channelToExport;
     }
 
-    private function getMaintenanceConfiguration(InputInterface $input):void
+    private function getMaintenanceConfiguration(InputInterface $input): void
     {
         $this->maintenanceConfiguration = $this->configurationFactory->get();
         $this->maintenanceConfiguration->setChannels($this->getChannels());
@@ -76,18 +78,20 @@ final class EnableMaintenanceCommand extends Command
         }
     }
 
-    private function isMaintenanceConfigurationEnabled():bool
+    private function isMaintenanceConfigurationEnabled(): bool
     {
-        if (count($this->maintenanceConfiguration->getChannels()) > 1){
+        if (count($this->maintenanceConfiguration->getChannels()) > 1) {
             return false;
         }
+
         return true;
     }
 
-    private function defineOutputMessage(OutputInterface $output):void
+    private function defineOutputMessage(OutputInterface $output): void
     {
-        if ($this->maintenanceConfiguration->isEnabled()){
+        if ($this->maintenanceConfiguration->isEnabled()) {
             $output->writeln($this->translator->trans('maintenance.ui.message_enabled'));
+
             return;
         }
         $output->writeln($this->translator->trans('maintenance.ui.cannot_enable_maintenance'));
